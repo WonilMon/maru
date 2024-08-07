482bc51b8d74ea6e666ce3f48db19c704349a5ef
asus <asus@DESKTOP-T3DJ34V>
2024-08-07 16:43:17 +0900

    테스트

--- src/main/java/kr/co/softsoldesk/controller/ShopController.java
+++ src/main/java/kr/co/softsoldesk/controller/ShopController.java
@@ -1,6 +1,8 @@
 package kr.co.softsoldesk.controller;
 
-import org.springframework.stereotype.Controller;	
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -8,8 +10,13 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("/shop")
 public class ShopController {
 
+	@Autowired
+	private IconService iconService;
+	
 	@GetMapping("/shop")
-	private String shop() {
+	private String shop(Model model) {
+		List<Icon> icons = iconService.getIcons();
+		model.addAttribute("icons",icons);		
 		return "shop/shop";
 	}
 }
