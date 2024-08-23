a573420a8dd2f19340279329841915761c2cbb0f
user <user@DESKTOP-0KRUBO7>
2024-08-23 16:00:31 +0900

    240823 이미지추가 yelim

--- src/main/java/kr/co/softsoldesk/controller/RestApiController.java
+++ src/main/java/kr/co/softsoldesk/controller/RestApiController.java
@@ -26,6 +26,7 @@ import kr.co.softsoldesk.service.UserService;
 
 @RestController
 @PropertySource("/WEB-INF/properties/option.properties")
+@PropertySource("/WEB-INF/properties/option2.properties")
 public class RestApiController {
 
 	@Resource(name = "loginUserBean")
